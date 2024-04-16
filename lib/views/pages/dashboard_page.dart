6ac7a3505842ec96391118375d2dcb0172e219c5
phiphiphi2k1 <philonnni@gmail.com>
2024-04-16 15:58:23 +0700

    update: dashboard

--- lib/views/pages/dashboard_page.dart
+++ lib/views/pages/dashboard_page.dart
@@ -1,12 +1,13 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
-import '/utils/line_chart.dart';
 import '/controllers/dashboard_controller.dart';
 import '/views/pages/widget/widget_dashboard.dart';
+import 'widget/dashboard_1.dart';
+import 'widget/dashboard_2.dart';
+import 'widget/dashboard_3.dart';
+import 'widget/dashboard_4.dart';
 
 class DashboardView extends StatelessWidget {
   DashboardView({super.key});
@@ -26,72 +27,6 @@ class DashboardView extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    // Container(
-                    //   padding: const EdgeInsets.only(left: 5, right: 5),
-                    //   width: 170,
-                    //   height: 70,
-                    //   alignment: Alignment.center,
-                    //   child: TextFormField(
-                    //     readOnly: true,
-                    //     decoration: InputDecoration(
-                    //       label: Obx(
-                    //         () => Text(
-                    //           ' ${_dashboardController.selectedDateStrStart}',
-                    //           style: const TextStyle(color: Colors.black),
-                    //           maxLines: 1,
-                    //         ),
-                    //       ),
-                    //       prefixIcon: const Icon(Icons.calendar_today),
-                    //     ),
-                    //     onTap: () async {
-                    //       final DateTime? picked = await showDatePicker(
-                    //         context: context,
-                    //         initialDate:
-                    //             _dashboardController.selectedDateStart.value,
-                    //         firstDate:  DateTime.now().subtract(const Duration(days: 365)),
-                    //         lastDate:  DateTime.now(),
-                    //       );
-                    //       if (picked != null) {
-                    //         _dashboardController.selectedDateStart.value =
-                    //             picked;
-                    //         _dashboardController.selectedDateStrStart.value =
-                    //             DateFormat('dd-MM-yyyy').format(picked);
-                    //       }
-                    //     },
-                    //   ),
-                    // ),
-                    // Container(
-                    //   alignment: Alignment.center,
-                    //   padding: const EdgeInsets.only(left: 5, right: 5),
-                    //   width: 170,
-                    //   height: 70,
-                    //   child: TextFormField(
-                    //     decoration: InputDecoration(
-                    //       label: Obx(
-                    //         () => Text(
-                    //           ' ${_dashboardController.selectedDateStrEnd}',
-                    //           style: const TextStyle(color: Colors.black),
-                    //           maxLines: 1,
-                    //         ),
-                    //       ),
-                    //       prefixIcon: const Icon(Icons.calendar_today),
-                    //     ),
-                    //     onTap: () async {
-                    //       final DateTime? picked = await showDatePicker(
-                    //         context: context,
-                    //         initialDate:
-                    //             _dashboardController.selectedDateEnd.value,
-                    //         firstDate: DateTime.now().subtract(const Duration(days: 365)),
-                    //         lastDate:  DateTime.now(),
-                    //       );
-                    //       if (picked != null) {
-                    //         _dashboardController.selectedDateEnd.value = picked;
-                    //         _dashboardController.selectedDateStrEnd.value =
-                    //             DateFormat('dd-MM-yyyy').format(picked);
-                    //       }
-                    //     },
-                    //   ),
-                    // ),
                     GestureDetector(
                       onTap: () async {
                         final DateTimeRange? picked = await showDateRangePicker(
@@ -183,140 +118,47 @@ class DashboardView extends StatelessWidget {
               ),
             ],
           ),
-          Row(children: [
-            Expanded(
-              child: Card(
-                margin: const EdgeInsets.only(
-                    left: 10, top: 10, right: 10, bottom: 10),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(children: [
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Your Chart Title',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 450,
-                      child: LineChart(
-                        // isShowingMainData ? sampleData1 : sampleData2,
-                        sampleData2,
-                        duration: const Duration(milliseconds: 250),
-                      ),
-                    ),
-                  ]),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Card(
-                margin: const EdgeInsets.only(
-                    left: 10, top: 10, right: 10, bottom: 10),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(children: [
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Your Chart Title',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 450,
-                      child: LineChart(
-                        // isShowingMainData ? sampleData1 : sampleData2,
-                        sampleData2,
-                        duration: const Duration(milliseconds: 250),
-                      ),
-                    ),
-                  ]),
-                ),
-              ),
-            ),
+          const Row(children: [
+            Expanded(child: PointDashboard1()),
+            Expanded(child: PointDashboard2()),
           ]),
-          Row(children: [
-            Expanded(
-              child: Card(
-                margin: const EdgeInsets.only(
-                    left: 10, top: 10, right: 10, bottom: 10),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(children: [
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Your Chart Title',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 450,
-                      child: LineChart(
-                        // isShowingMainData ? sampleData1 : sampleData2,
-                        sampleData2,
-                        duration: const Duration(milliseconds: 250),
-                      ),
-                    ),
-                  ]),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Card(
-                margin: const EdgeInsets.only(
-                    left: 10, top: 10, right: 10, bottom: 10),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(children: [
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Your Chart Title',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 450,
-                      child: LineChart(
-                        // isShowingMainData ? sampleData1 : sampleData2,
-                        sampleData2,
-                        duration: const Duration(milliseconds: 250),
-                      ),
-                    ),
-                  ]),
-                ),
-              ),
-            ),
+          const Row(children: [
+            Expanded(child: PointDashboard3()),
+            Expanded(child: PointDashboard4()),
           ]),
         ]),
       ),
     );
   }
 }
+
+// child: Card(
+//                 margin: const EdgeInsets.only(
+//                     left: 10, top: 10, right: 10, bottom: 10),
+//                 child: Container(
+//                   padding: const EdgeInsets.all(10),
+//                   child: Column(children: [
+//                     const Padding(
+//                       padding: EdgeInsets.only(bottom: 10),
+//                       child: Align(
+//                         alignment: Alignment.centerLeft,
+//                         child: Text(
+//                           'Your Chart Title',
+//                           style: TextStyle(
+//                             fontSize: 24,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(
+//                       height: 450,
+//                       child: LineChart(
+//                         // isShowingMainData ? sampleData1 : sampleData2,
+//                         sampleData2,
+//                         duration: const Duration(milliseconds: 250),
+//                       ),
+//                     ),
+//                   ]),
+//                 ),
+//               ),
---
+++ lib/views/pages/widget/dashboard_1.dart
@@ -0,0 +1,213 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class PointDashboard1 extends StatelessWidget {
+  const PointDashboard1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        // color: Colors.red,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text(
+              'Tổng kết giao dịch điểm (6 tháng)',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 50),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: BarChart(
+                BarChartData(
+                  barTouchData: barTouchData,
+                  titlesData: titlesData,
+                  borderData: borderData,
+                  barGroups: barGroups,
+                  gridData: const FlGridData(show: false),
+                  alignment: BarChartAlignment.spaceAround,
+                  maxY: 100,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BarTouchData get barTouchData => BarTouchData(
+        enabled: false,
+        touchTooltipData: BarTouchTooltipData(
+          tooltipBgColor: Colors.transparent,
+          tooltipPadding: EdgeInsets.zero,
+          tooltipMargin: 8,
+          getTooltipItem: (
+            BarChartGroupData group,
+            int groupIndex,
+            BarChartRodData rod,
+            int rodIndex,
+          ) {
+            return BarTooltipItem(
+              rod.toY.round().toString(),
+              const TextStyle(
+                color: Colors.cyan,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
+      );
+
+  Widget getTitles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.black,
+      // fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = 'Mn';
+        break;
+      case 1:
+        text = 'Te';
+        break;
+      case 2:
+        text = 'Wd';
+        break;
+      case 3:
+        text = 'Tu';
+        break;
+      case 4:
+        text = 'Fr';
+        break;
+      case 5:
+        text = 'St';
+        break;
+      case 6:
+        text = 'Sn';
+        break;
+      default:
+        text = '';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+
+  FlTitlesData get titlesData => FlTitlesData(
+        show: true,
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            getTitlesWidget: getTitles,
+          ),
+        ),
+        leftTitles: const AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: 20,
+            reservedSize: 35,
+          ),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+      );
+
+  FlBorderData get borderData => FlBorderData(
+        show: false,
+      );
+
+  LinearGradient get _barsGradient => const LinearGradient(
+        colors: [
+          Colors.blueAccent,
+          Colors.cyan,
+        ],
+        begin: Alignment.bottomCenter,
+        end: Alignment.topCenter,
+      );
+
+  List<BarChartGroupData> get barGroups => [
+        BarChartGroupData(
+          x: 0,
+          barRods: [
+            BarChartRodData(
+              toY: 7,
+              gradient: _barsGradient,
+            )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 1,
+          barRods: [
+            BarChartRodData(
+              toY: 100,
+              gradient: _barsGradient,
+            )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 2,
+          barRods: [
+            BarChartRodData(
+              toY: 14,
+              gradient: _barsGradient,
+            )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 3,
+          barRods: [
+            BarChartRodData(
+              toY: 15,
+              gradient: _barsGradient,
+            )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 4,
+          barRods: [
+            BarChartRodData(
+              toY: 13,
+              gradient: _barsGradient,
+            )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 5,
+          barRods: [
+            BarChartRodData(
+              toY: 10,
+              gradient: _barsGradient,
+            )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+        BarChartGroupData(
+          x: 6,
+          barRods: [
+            BarChartRodData(
+              toY: 16,
+              gradient: _barsGradient,
+            )
+          ],
+          showingTooltipIndicators: [0],
+        ),
+      ];
+}
