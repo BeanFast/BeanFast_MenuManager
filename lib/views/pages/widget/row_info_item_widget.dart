d1fb9a1c53c88f1f336a09b53fd1ec69f8fca7db
phiphiphi2k1 <philonnni@gmail.com>
2024-05-14 19:30:42 +0700

    update

--- lib/views/pages/widget/row_info_item_widget.dart
+++ lib/views/pages/widget/row_info_item_widget.dart
@@ -1,34 +1,34 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
 
-class RowInfoItemWidget extends StatelessWidget {
-  const RowInfoItemWidget({super.key, required this.title, required this.data});
-  final String title;
-  final String data;
+// class RowInfoItemWidget extends StatelessWidget {
+//   const RowInfoItemWidget({super.key, required this.title, required this.data});
+//   final String title;
+//   final String data;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-              child: Text(
-            title,
-           style:  Get.textTheme.bodyMedium
-          )),
-          Expanded(
-            child: Text(
-              data,
-              textAlign: TextAlign.right,
-              style:  Get.textTheme.bodyMedium
-            ),
-          ),
-          const SizedBox(height: 10),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.only(bottom: 10),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           SizedBox(
+//               child: Text(
+//             title,
+//            style:  Get.textTheme.bodyMedium
+//           )),
+//           Expanded(
+//             child: Text(
+//               data,
+//               textAlign: TextAlign.right,
+//               style:  Get.textTheme.bodyMedium
+//             ),
+//           ),
+//           const SizedBox(height: 10),
+//         ],
+//       ),
+//     );
+//   }
+// }
